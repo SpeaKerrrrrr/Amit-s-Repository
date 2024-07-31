4bd6bc9725b0100d484b31b0d9805831c3f93e2b
SpeaKerrrrrr <amit40445@gmail.com>
2024-07-31 16:06:12 +0300

    Ortal adjusted + chat tried to add register\login

--- WebProject/MyWeb/urls.py
+++ WebProject/MyWeb/urls.py
@@ -1,2 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+
+]
+
